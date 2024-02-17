e7b70dd8cf4db873b2ed5daf8fc770d3306f25a2
SobakaBekka <st69704@upce.cz>
2024-02-18 00:09:37 +0100

    done all taska

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
-    public void roundNumber(float numberToBeRounded) {
+    public void roundNumber(float numberToBeRounded){
+
+        long a = Math.round(numberToBeRounded);
+        System.out.println(numberToBeRounded);
+
     }
 }
